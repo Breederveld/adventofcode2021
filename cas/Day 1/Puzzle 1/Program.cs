c46d0c5c1c60c720f543108463d6842a981c042c
casvangool <casjevangool@gmail.com>
2021-12-01 20:23:07 +0100

    Awnser Cas Day 1

---
+++ cas/Day 1/Puzzle 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace codeadvent
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string input = Input.input;
+            List<int> numbers = input.Split("\r\n").Select(item => Convert.ToInt32(item)).ToList();
+            int amountIncreased = 0;
+            int amountProcessed = 0;
+            for(int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i - 1] < numbers[i])
+                    amountIncreased++;
+                amountProcessed++;
+            }
+            Console.WriteLine(amountIncreased);
+            Console.WriteLine(amountProcessed);
+        }
+    }
+}
