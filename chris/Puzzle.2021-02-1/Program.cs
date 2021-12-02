534842a65fceb44c59e657d25c2082db789b4955
Chris Breederveld <chris.breederveld@strukton.com>
2021-12-02 05:08:04 +0000

    Chris solutions day 2

---
+++ chris/Puzzle.2021-02-1/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Puzzle_2021_02_1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var rootFolder = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            var input = File.ReadAllText(Path.Combine(rootFolder, "input.txt"));
+
+            var strings = input.Trim().Split("\n").ToArray();
+            //var groups = input.Trim().Split("\n\n").Select(grp => grp.Split("\n").ToArray()).ToArray();
+            //var ints = strings.Where(st => !string.IsNullOrWhiteSpace(st)).Select(st => int.Parse(st)).ToArray();
+
+            var x = 0;
+            var d = 0;
+            foreach (var s in strings)
+            {
+                var parts = s.Split(' ');
+                var cnt = int.Parse(parts[1]);
+                switch (parts[0])
+                {
+                    case "forward":
+                        x += cnt;
+                        break;
+                    case "up":
+                        d -= cnt;
+                        break;
+                    case "down":
+                        d += cnt;
+                        break;
+                }
+            }
+
+            Console.WriteLine(x * d);
+            await Task.FromResult(0);
+        }
+    }
+}
