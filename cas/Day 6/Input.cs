f805d24f83fcd84061c48738fb6d1fbd2297e1db
Cas van Gool <cas.vangool@strukton.com>
2021-12-06 11:05:02 +0100

    Day 6 Cas

---
+++ cas/Day 6/Input.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCode
+{
+    public static class Input
+    {
+        public static Dictionary<int, decimal> DaysLaternFishMap()
+        {
+            string[] laternfish = data.Split(',');
+            Dictionary<int, decimal> dict = new Dictionary<int, decimal>();
+            foreach(string fish in laternfish)
+            {
+                int dayoffish = int.Parse(fish);
+                if (!dict.ContainsKey(dayoffish))
+                {
+                    dict.Add(dayoffish, 1);
+                } else
+                {
+                    dict[dayoffish] += 1;
+                }
+            }
+            return dict;
+        }
+
+        public static string data => @"1,1,1,1,2,1,1,4,1,4,3,1,1,1,1,1,1,1,1,4,1,3,1,1,1,5,1,3,1,4,1,2,1,1,5,1,1,1,1,1,1,1,1,1,1,3,4,1,5,1,1,1,1,1,1,1,1,1,3,1,4,1,1,1,1,3,5,1,1,2,1,1,1,1,4,4,1,1,1,4,1,1,4,2,4,4,5,1,1,1,1,2,3,1,1,4,1,5,1,1,1,3,1,1,1,1,5,5,1,2,2,2,2,1,1,2,1,1,1,1,1,3,1,1,1,2,3,1,5,1,1,1,2,2,1,1,1,1,1,3,2,1,1,1,4,3,1,1,4,1,5,4,1,4,1,1,1,1,1,1,1,1,1,1,2,2,4,5,1,1,1,1,5,4,1,3,1,1,1,1,4,3,3,3,1,2,3,1,1,1,1,1,1,1,1,2,1,1,1,5,1,3,1,4,3,1,3,1,5,1,1,1,1,3,1,5,1,2,4,1,1,4,1,4,4,2,1,2,1,3,3,1,4,4,1,1,3,4,1,1,1,2,5,2,5,1,1,1,4,1,1,1,1,1,1,3,1,5,1,2,1,1,1,1,1,4,4,1,1,1,5,1,1,5,1,2,1,5,1,1,1,1,1,1,1,1,1,1,1,1,3,2,4,1,1,2,1,1,3,2";
+    }
+}
---
+++ cas/Day 6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventofCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, decimal> daysandamountoffish = Input.DaysLaternFishMap();
+            Dictionary<int, decimal> startofday = new Dictionary<int, decimal>(daysandamountoffish);
+            for (int i = 0; i < 256; i++)
+            {
+                Dictionary<int, decimal> endofday = new Dictionary<int, decimal>();
+                foreach(KeyValuePair<int,decimal> fish in startofday)
+                {
+                    if(fish.Key > 0)
+                    {
+                        if (endofday.ContainsKey(fish.Key - 1))
+                        {
+                            endofday[(fish.Key - 1)] += fish.Value;
+                        }
+                        else
+                        {
+                            endofday.Add((fish.Key - 1), fish.Value);
+                        }
+                    }
+                    if(fish.Key == 0)
+                    {
+                        if (endofday.ContainsKey(6))
+                        {
+                            endofday[6] += fish.Value;
+                        }
+                        else
+                        {
+                            endofday.Add(6, fish.Value);
+                        }
+                        if (endofday.ContainsKey(8))
+                        {
+                            endofday[8] += fish.Value;
+                        }
+                        else
+                        {
+                            endofday.Add(8, fish.Value);
+                        }
+                    }
+                }
+                startofday = new Dictionary<int, decimal>(endofday);
+            }
+            Console.WriteLine(startofday.Select(item => item.Value).Sum(item => item));
+        }
+    }
+}
